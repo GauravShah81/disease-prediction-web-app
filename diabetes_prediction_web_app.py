59f113fda6607c8fb12961676d3e5a2adbcafc16
Gaurav Shah <gauravshah081@gmail.com>
2026-03-23 20:21:57 +0530

    added reset button

--- diabetes_prediction_web_app.py
+++ diabetes_prediction_web_app.py
@@ -29,6 +29,14 @@ def load_models():
 
 diabetes_loaded_model,parkinson_loaded_model,heart_disease_loaded_model,scaler = load_models()
 
+def reset_form(prefix):
+    keys_to_reset = [key for key in st.session_state if key.startswith(prefix)]
+    for key in keys_to_reset:
+        if isinstance(st.session_state[key], (int, float)):
+            st.session_state[key] = 0
+        else:
+            st.session_state[key] = ""
+
 def predict(model,input_data,columns,scaler = None):
     df = pd.DataFrame([input_data],columns=columns)
 
@@ -116,35 +124,47 @@ def home():
     st.title("Welcome To Multi-Disease Prediction Web App.")
     st.subheader("\n\nPlease Select a disease from sidebar for diagnoses")
     st.write("Disease availabe for diagnoses are:\n1. Diabetes \n2. Parkinson's disease \n3. Heart Disease ")
+
 def Diabetes_button():
     # giving a title
     st.title("Diabetes Prediction")
 
+    # Reset Logic before creating form
+    if "reset_diab" in st.session_state and st.session_state["reset_diab"]:
+        reset_form("diab_")
+        st.session_state["reset_diab"] = False  # reset the flag
+
+    # Form Creation
     with st.form("Diabetes Form"):
 
         # getting the input data from user
         col = st.columns(4)
 
         with col[0]:
-            Pregnancies = st.number_input("Number of Pregnancies:", min_value=0,step=1)
+            Pregnancies = st.number_input("Number of Pregnancies:", min_value=0,step=1,key="diab_preg")
         with col[1]:
-            Glucose = st.number_input("Glucose Level:", min_value=0,step=1)
+            Glucose = st.number_input("Glucose Level:", min_value=0,step=1,key="diab_gluc")
         with col[2]:
-            BloodPressure = st.number_input("BloodPressure value:", min_value=0,step=1)
+            BloodPressure = st.number_input("BloodPressure value:", min_value=0,step=1,key="diab_BP")
         with col[3]:
-            SkinThickness = st.number_input("Skin Thickness Value:", min_value=0,step=1)
+            SkinThickness = st.number_input("Skin Thickness Value:", min_value=0,step=1,key="diab_ST")
 
         cols = st.columns(4)
         with cols[0]:
-            Insulin = st.number_input("Insulin Level:", min_value=0,step=1)
+            Insulin = st.number_input("Insulin Level:", min_value=0,step=1,key="diab_Ins")
         with cols[1]:
-            BMI = st.number_input("BMI Value:", min_value=0.0,step=0.1)
+            BMI = st.number_input("BMI Value:", min_value=0.0,step=0.1,key="diab_bmi")
         with cols[2]:
-            DiabetesPedigreeFunction = st.number_input("Diabetes Pedigree Function:", min_value=0.0,step=0.001)
+            DiabetesPedigreeFunction = st.number_input("Diabetes Pedigree Function:", min_value=0.0,step=0.001,key="diab_pedigfunc")
         with cols[3]:
-            Age = st.number_input("Age of the Person:", min_value=0,step=1)
+            Age = st.number_input("Age of the Person:", min_value=0,step=1,key="diab_age")
 
         submitted = st.form_submit_button("Predict")
+    
+    # Reset Button
+    if st.button("Reset"):
+        st.session_state["reset_diab"] = True
+        st.rerun()  # rerun the app to apply cleared values
 
     # prediction
     if submitted:
@@ -161,71 +181,82 @@ def Parkinson_button():
     # giving a title
     st.title("Parkinson's disease Prediction")
 
+    # Reset Logic before creating form
+    if "reset_parkinson" in st.session_state and st.session_state["reset_parkinson"]:
+        reset_form("park_")
+        st.session_state["reset_parkinson"] = False # reset flag
+
+    # Form Creation
     with st.form("Parkinson Form"):
         # getting the input from the user
         cols = st.columns(4)
 
         with cols[0]:
-            MDVP_Fo = st.number_input("MDVP:Fo(Hz) value:", min_value=0.0)
+            MDVP_Fo = st.number_input("MDVP:Fo(Hz) value:", min_value=0.0,key="park_mdfo")
         with cols[1]:
-            MDVP_Fhi = st.number_input("MDVP:Fhi(Hz) value:", min_value=0.0)
+            MDVP_Fhi = st.number_input("MDVP:Fhi(Hz) value:", min_value=0.0,key="park_mdfhi")
         with cols[2]:
-            MDVP_Flo = st.number_input("MDVP:Flo(Hz) value:", min_value=0.0)
+            MDVP_Flo = st.number_input("MDVP:Flo(Hz) value:", min_value=0.0,key="park_mdflo")
         with cols[3]:
-            MDVP_Jitter = st.number_input("MDVP:Jitter(%) Value:", min_value=0.0)
+            MDVP_Jitter = st.number_input("MDVP:Jitter(%) Value:", min_value=0.0,key="park_mdjit")
         
         cols = st.columns(4)
 
         with cols[0]:
-            MDVP_Jitter_abs = st.number_input("MDVP:Jitter(Abs) value:", min_value=0.0)
+            MDVP_Jitter_abs = st.number_input("MDVP:Jitter(Abs) value:", min_value=0.0,key="park_mdjitabs")
         with cols[1]:
-            MDVP_RAP = st.number_input("MDVP:RAP Value:", min_value=0.0)
+            MDVP_RAP = st.number_input("MDVP:RAP Value:", min_value=0.0,key="park_mdrap")
         with cols[2]:
-            MDVP_PPQ = st.number_input("MDVP:PPQ Value:", min_value=0.0)
+            MDVP_PPQ = st.number_input("MDVP:PPQ Value:", min_value=0.0,key="park_mdppq")
         with cols[3]:
-            Jitter_DDP = st.number_input("Jitter:DDP Value:", min_value=0.0)
+            Jitter_DDP = st.number_input("Jitter:DDP Value:", min_value=0.0,key="park_jitddp")
         
         cols = st.columns(4)
 
         with cols[0]:
-            MDVP_Shimmer = st.number_input("MDVP:Shimmer Value:", min_value=0.0)
+            MDVP_Shimmer = st.number_input("MDVP:Shimmer Value:", min_value=0.0,key="park_mdshi")
         with cols[1]:
-            MDVP_Shimmer_dB = st.number_input("MDVP:Shimmer(dB) Value:", min_value=0.0)
+            MDVP_Shimmer_dB = st.number_input("MDVP:Shimmer(dB) Value:", min_value=0.0,key="park_mdshidb")
         with cols[2]:
-            Shimmer_APQ3 = st.number_input("Shimmer:APQ3 Value:", min_value=0.0)
+            Shimmer_APQ3 = st.number_input("Shimmer:APQ3 Value:", min_value=0.0,key="park_shiaq3")
         with cols[3]:
-            Shimmer_APQ5 = st.number_input("Shimmer:APQ5 Value:", min_value=0.0)
+            Shimmer_APQ5 = st.number_input("Shimmer:APQ5 Value:", min_value=0.0,key="park_shiaqq5")
 
         cols = st.columns(4)
 
         with cols[0]:
-            MDVP_APQ = st.number_input("MDVP:APQ Value:", min_value=0.0)
+            MDVP_APQ = st.number_input("MDVP:APQ Value:", min_value=0.0,key="park_mdaq")
         with cols[1]:
-            Shimmer_DDA = st.number_input("Shimmer:DDA Value:", min_value=0.0)
+            Shimmer_DDA = st.number_input("Shimmer:DDA Value:", min_value=0.0,key="park_shidda")
         with cols[2]:
-            NHR = st.number_input("NHR Value:", min_value=0.0)
+            NHR = st.number_input("NHR Value:", min_value=0.0,key="park_nhr")
         with cols[3]:
-            HNR = st.number_input("HNR Value:", min_value=0.0)
+            HNR = st.number_input("HNR Value:", min_value=0.0,key="park_hnr")
 
         cols = st.columns(4)
 
         with cols[0]:
-            RPDE = st.number_input("RPDE Value:", min_value=0.0)
+            RPDE = st.number_input("RPDE Value:", min_value=0.0,key="park_rpde")
         with cols[1]:
-            DFA = st.number_input("DFA Value:", min_value=0.0)
+            DFA = st.number_input("DFA Value:", min_value=0.0,key="park_dfa")
         with cols[2]:
-            spread1 = st.number_input("spread1 Value:", min_value=0.0)
+            spread1 = st.number_input("spread1 Value:", min_value=0.0,key="park_sp1")
         with cols[3]:
-            spread2 = st.number_input("spread2 Value:", min_value=0.0)
+            spread2 = st.number_input("spread2 Value:", min_value=0.0,key="park_sp2")
 
         cols = st.columns(4)
 
         with cols[0]:
-            D2 = st.number_input("D2 Value:", min_value=0.0)
+            D2 = st.number_input("D2 Value:", min_value=0.0,key="park_d2")
         with cols[1]:
-            PPE = st.number_input("PPE Value:", min_value=0.0)
+            PPE = st.number_input("PPE Value:", min_value=0.0,key="park_ppe")
 
         submitted = st.form_submit_button("Predict")
+
+    # Reset form
+    if st.button("Reset"):
+        st.session_state["reset_parkinson"] = True
+        st.rerun() # rerun the app to apply cleared values
 
     # prediction
 
@@ -238,52 +269,63 @@ def Parkinson_button():
 def heart_button():
     # giving a title
     st.title("Heart Disease Prediction")
+
+    # Reset before creating form
+    if "reset_heart" in st.session_state and st.session_state["reset_heart"]:
+        reset_form("heart_")
+        st.session_state["reset_heart"] = False #reset flag
     
+    # Form Creation
     with st.form("Heart Disease Form"):
         # getting the input from the user
         cols = st.columns(4)
 
         with cols[0]:
-            age = st.number_input("Age of the person:", min_value=0)
+            age = st.number_input("Age of the person:", min_value=0,key="heart_age")
         with cols[1]:
             sex = st.selectbox("Sex", ["Female", "Male"])            
             sex_map = {"Female": 0 , "Male":1}
             sex = sex_map[sex]
         with cols[2]:
-            cp = st.number_input("cp value:", min_value=0)
+            cp = st.number_input("cp value:", min_value=0,key="heart_cp")
         with cols[3]:
-            trestbps = st.number_input("trestbps Value:", min_value=0)
+            trestbps = st.number_input("trestbps Value:", min_value=0,key="heart_tres")
 
         cols = st.columns(4)
 
         with cols[0]:
-            chol = st.number_input("chol Value:", min_value=0)
+            chol = st.number_input("chol Value:", min_value=0,key="heart_chol")
         with cols[1]:
-            fbs = st.number_input("fbs Value:", min_value=0)
+            fbs = st.number_input("fbs Value:", min_value=0,key="heart_fbs")
         with cols[2]:
-            restecg = st.number_input("restecg Value:", min_value=0)
+            restecg = st.number_input("restecg Value:", min_value=0,key="heart_rest")
         with cols[3]:
-            thalach = st.number_input("thalach Value:", min_value=0)
+            thalach = st.number_input("thalach Value:", min_value=0,key="heart_thal")
 
         cols = st.columns(4)
 
         with cols[0]:
-            exang = st.number_input("exang Value:", min_value=0)
+            exang = st.number_input("exang Value:", min_value=0,key="heart_exang")
         with cols[1]:
-            oldpeak = st.number_input("oldpeak Value:", min_value=0)
+            oldpeak = st.number_input("oldpeak Value:", min_value=0,key="heart_oldp")
         with cols[2]:
-            slope = st.number_input("slope Value:", min_value=0)
+            slope = st.number_input("slope Value:", min_value=0,key="heart_slp")
         with cols[3]:
-            ca = st.number_input("ca Value:", min_value=0)
+            ca = st.number_input("ca Value:", min_value=0,key="heart_ca")
         
         cols = st.columns(4)
 
         with cols[0]:
-            thal = st.number_input("thal Value:", min_value=0)
+            thal = st.number_input("thal Value:", min_value=0,key="heart_th")
 
         submitted = st.form_submit_button("Predict")
 
-     # prediction
+    # Reset Form
+    if st.button("Reset"):
+        st.session_state["reset_heart"] = True
+        st.rerun() # rerun the app to apply cleared values
+
+    # prediction
 
     if submitted:
         if age < 1:
